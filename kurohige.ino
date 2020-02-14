3063b75d1c10aa8c9b06ec9dafd8f169d8976d20
Shintaro Tamefusa <tameshin@gmail.com>
2020-02-14 23:00:18 +0900

    update for prod.

--- kurohige.ino
+++ kurohige.ino
@@ -1,38 +1,55 @@
 #include <Servo.h>
 
+#define MODE_STAY 0
+#define MODE_JUMP 1
+#define MODE_BEFORE 9
+
 Servo servo1, servo2;
 int num1 = 0, num2 = 0;
 int val = 0, pre = 0, diff = 0;
 int cnt = 0;
 int vals[3] = {0, 0, 0};
+int mode = MODE_STAY;
+unsigned long jump_start_time = 0;
  
 void setup(){
   servo1.attach(7);
   servo2.attach(8);
-  num1 = 100;
+  num1 = 80;
+  num2 = 80;
   motor1(num1);
-  num2 = 60;
   motor2(num2);
   Serial.begin(9600);
+  
+  jump_start_time = millis();
+  mode = MODE_BEFORE;
 }
 
 void loop(){
-  //val = analogRead(0);
   vals[cnt] = analogRead(0);
   val = calcAve();
   diff = val - pre;
   Serial.println(diff);
    
-  if (diff > 200) {
-    num1 = 60;
-    num2 = 100;
+  if (diff > 100 && mode == MODE_STAY) {
+    mode = MODE_JUMP;
+    Serial.println("JUMP!!!!");
+    jump_start_time = millis();
+    num1 = 40;
+    num2 = 120;
   } 
-  else if (diff < -200) {
-    num1 = 100;
-    num2 = 60;
+  else if (diff < -100 && mode == MODE_STAY) {
+    Serial.println("STAY!!!!");
+    num1 = 80;
+    num2 = 80;
   }
   motor1(num1);
   motor2(num2);
+
+  if ((mode == MODE_JUMP || mode == MODE_BEFORE) && millis() - jump_start_time > 300) {
+    mode = MODE_STAY;
+    Serial.println("RESET MODE");
+  }
 
   pre = val;
   cnt = (cnt+1)%3;
@@ -56,4 +73,3 @@ int calcAve()
   }
   return (int)tmp/3;
 }
-
